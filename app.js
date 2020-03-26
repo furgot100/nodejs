3ff8d617bd80c22d327e1052ba16220d258e9816
Francis Tsang <frtsang40@gmail.com>
2020-03-26 11:54:59 -0700

    finished with os module

--- app.js
+++ app.js
@@ -2,10 +2,26 @@
 
 // log('message');
 
-const path = require('path')
 
-var pathObj = path.parse(__filename)
 
-console.log(pathObj)
+// Path module
+// const path = require('path')
 
+// var pathObj = path.parse(__filename)
+
+// console.log(pathObj)
+
+
+// OS module
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+// console.log('Total Memory: ' + totalMemory);
+
+// Template string
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
 
