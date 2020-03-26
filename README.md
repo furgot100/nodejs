f742206e8ffcaa6d2e732bb97215ea79a3a1da7d
Francis Tsang <frtsang40@gmail.com>
2020-03-26 13:11:34 -0700

    finished with http module

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tutorial for Node.js
+This is a repo for a tutorial I've done for Node.js 
+I followed a tutorial by Mosh form Programming with Mosh
+You can find the video [here](youtube.com/watch?v=TlB_eWDSMt4&list=PLTjRvDozrdlynYXGUfyyMZdrQ0Sz27aud&index=5&t=0s)
--- app.js
+++ app.js
@@ -38,17 +38,38 @@
 // });
 
 // EVENT MODULE
-const EventEmitter = require('events'); //EventEmmiter is a class
+// const EventEmitter = require('events'); //EventEmmiter is a class
 
 
-const Logger = require('./logger');
-const logger = new Logger();
+// const Logger = require('./logger');
+// const logger = new Logger();
 
 
-//listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
+// //listener
+// logger.on('messageLogged', (arg) => {
+//     console.log('Listener called', arg);
+// });
+
+// logger.log('message');
+
+// HTTP MODULE
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
 });
 
-logger.log('message');
 
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
