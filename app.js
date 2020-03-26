fab4fbe0b0dde5ce3b8c758415480bc9e78cbddd
Francis Tsang <frtsang40@gmail.com>
2020-03-26 12:01:06 -0700

    finished with file system

--- app.js
+++ app.js
@@ -13,15 +13,27 @@
 
 
 // OS module
-const os = require('os');
+// const os = require('os');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
 
-// console.log('Total Memory: ' + totalMemory);
+// // console.log('Total Memory: ' + totalMemory);
 
-// Template string
+// // Template string
 
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+
+//  FILE SYSTEM MODULE
+// const fs = require('fs');
+
+// // const files =fs.readdirSync('./');
+// // console.log(files);
+
+// fs.readdir('./', function(err, files) {
+//     if (err) console.log('Error', err);
+//     else console.log('Result', files)
+// });
 
